72172d8c5837989f526670ef2a4f77fc90a4170f
Artem Kozlov <artem.art16@mail.ru>
2024-01-16 14:46:46 +0300

    init

---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,28 @@
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+
+    @Test
+    void successfulSearchTest() {
+        open("https://google.com"); // открыть google
+
+        $("textarea[name=q]").setValue("selenide").pressEnter(); // ввести в поле поиска selenide
+
+        $("div[id=search]").shouldHave(text("https://ru.selenide.org")); // проверить выдачу
+
+        sleep(2000);
+    }
+}
